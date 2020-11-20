007bf2a9e9436249fc7f5f3585bc226a599c32c5
Bombsztyl <kacpergumieniuk@gmail.com>
2020-11-20 20:47:28 +0100

    Complete

--- app.js
+++ app.js
@@ -19,10 +19,16 @@ const myIcon = L.icon({
    
 });
 
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png?{foo}', {foo: 'bar', attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>'}).addTo(map);
+// L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png?{foo}', {foo: 'bar', attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>'}).addTo(map);
+// //
+
+const googleStreets = L.tileLayer('http://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}',{
+    maxZoom: 20,
+    subdomains:['mt0','mt1','mt2','mt3']
+});
+
+googleStreets.addTo(map);
 const marker = L.marker([51.505, -0.09] ,{icon: myIcon}).addTo(map);
-
-
 
 async function initialize(url){
     const result = await fetch(url);
@@ -32,20 +38,32 @@ async function initialize(url){
 
 
     marker.setLatLng([lat, lng]);
-    map.setView([lat,lng], 16);
+    map.setView([lat,lng], 13);
 
     ipNumber.innerHTML = data.ip;
     locationName.innerHTML = ` ${data.location.country}, ${data.location.city}`;
     timezoneStat.innerHTML = data.location.timezone;
     ispStat.innerHTML = data.isp;
-
 }
 
 function Submit(){
     let input = document.getElementById("text-input").value;
     console.log(input);
+
+    if (input.match(/^\d/) === true) {
     initialize(`https://geo.ipify.org/api/v1?apiKey=at_OmkDkxIJ4Ayu8F9Ava8rCYMNDYVfd&ipAddress=${input}`);
+    }
+    else{
+        initialize(`https://geo.ipify.org/api/v1?apiKey=at_OmkDkxIJ4Ayu8F9Ava8rCYMNDYVfd&domain=${input}`)
+    }
 }
 
+document.getElementById("text-input").addEventListener("keypress", function(event) {
+    if (event.keyCode == 13){
+    event.preventDefault();
+    
+        document.getElementById("button").click();
+    }
+});
 
-initialize("https://geo.ipify.org/api/v1?apiKey=at_OmkDkxIJ4Ayu8F9Ava8rCYMNDYVfd&ipAddress=");
+initialize("https://geo.ipify.org/api/v1?apiKey=at_OmkDkxIJ4Ayu8F9Ava8rCYMNDYVfd");
